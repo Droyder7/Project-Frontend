4701b8ff05f417a87301f90f2aa52c813abbe911
Dipangshu Roy <57279309+Droyder7@users.noreply.github.com>
2020-10-13 17:58:24 +0530

    installed firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './Core/Home';
 
+/* Firebase SDK */
+import firebase from 'firebase/app';
+import 'firebase/firestore';  // For database
+import 'firebase/auth'; // For user authentication
+
+/* Firebase Hooks */
+import { useAuthState } from "react-firebase-hooks/auth";
+
 function App() {
   return (
     <div className="App">
